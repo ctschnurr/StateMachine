18c936e6f122d7a732b6711352cb006c20ba2600
ctschnurr <113189402+ctschnurr@users.noreply.github.com>
2023-02-21 08:58:11 -0400

    Initial commit

---
+++ StateMachine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    internal class Program
+    {
+
+        static int state = 0;
+        // 0 = patrolling
+        // 1 = chasing
+        // 2 = searching
+        // 3 = attacking
+        // 4 = retreating
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, we're building a State Machine!");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
+        }
+    }
+}
